d9a143f7399bae125568734c8f848b731c25dc8a
Adam Gamble <adamgamble@gmail.com>
2012-01-10 22:59:51 -0600

    [AG] initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ngdale::Application.config.secret_token = '7c26473d25819a20b72cd6a12c499cdc1fd0dcd3d3a0d844020cf0c0cf015883caa99c2b3dd625d1079225efe25ebb1ae066049d2562276f89941f554ef77e96'
