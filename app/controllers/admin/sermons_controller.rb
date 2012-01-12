2fb6f8d0515d587885d9fdd25b0b41bb17e0f1fe
Adam Gamble <adamgamble@gmail.com>
2012-01-12 16:45:01 -0600

    [AG] added the edit action

--- app/controllers/admin/sermons_controller.rb
+++ app/controllers/admin/sermons_controller.rb
@@ -11,6 +11,21 @@ class Admin::SermonsController < Admin::BaseController
     @sermon = Sermon.new
   end
 
+  def edit
+    @sermon = Sermon.find(params[:id])
+  end
+
+  def update
+    @sermon = Sermon.find(params[:id])
+    if @sermon.update_attributes(params[:sermon])
+      flash[:notice] = "Sermon updated"
+      redirect_to admin_sermon_path(@sermon)
+    else
+      flash[:error] = "Error updating sermon"
+      render :action => :edit
+    end
+  end
+
   def create
     @sermon = Sermon.new(params[:sermon])
     if @sermon.save
@@ -20,5 +35,12 @@ class Admin::SermonsController < Admin::BaseController
       flash[:error] = "Error saving sermon"
       render :action => :new
     end
+  end
+
+  def destroy
+    @sermon = Sermon.find(params[:id])
+    @sermon.destroy
+    flash[:notice] = "Sermon Destroyed"
+    redirect_to :action => :index
   end
 end
