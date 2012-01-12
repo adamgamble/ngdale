a96621becba0036be945e852fe1fbc673e56a7ba
Adam Gamble <adamgamble@gmail.com>
2012-01-12 16:46:44 -0600

    [AG] fixed 500

--- app/models/sermon.rb
+++ app/models/sermon.rb
@@ -1,7 +1,7 @@
 class Sermon < ActiveRecord::Base
   has_attached_file :sermon,
     :storage => :s3,
-    :bucket => 'ngdale-#{Rails.env}',
+    :bucket => "ngdale-#{Rails.env}",
     :s3_credentials => {
     :access_key_id => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
